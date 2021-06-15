d933ceaa96a77e6ce37f71e84140821ac53f9605
Michael Pingel <mdpingel@gmail.com>
2021-06-15 12:54:11 -0700

    create player.js object and import it into game component

--- src/app/brick-breaker-game/brick-breaker-game.component.ts
+++ src/app/brick-breaker-game/brick-breaker-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Player from './js/player';
 import * as p5 from 'p5';
 
 @Component({
@@ -9,14 +10,17 @@ import * as p5 from 'p5';
 export class BrickBreakerGameComponent implements OnInit {
 
   ngOnInit(): void {
+    let player: Player;
     new p5(p => {
       p.setup = () => {
         const canvas = p.createCanvas(1000, 900);
         canvas.parent('game-holder');
+        player = new Player(p);
       }
 
       p.draw = () => {
         p.background(51);
+        player.show();
       }
     })
   }
---
+++ src/app/brick-breaker-game/js/player.js
@@ -0,0 +1,9 @@
+export default function Player(p) {
+  this.x = p.width/2;
+
+  this.show = function() {
+    p.fill(255);
+    p.rectMode(p.CENTER);
+    p.rect(this.x, p.height-100, 60, 20);
+  }
+}
